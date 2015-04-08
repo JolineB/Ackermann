a1ff75a4688b1c839d6b5d8464201b8b8ff665ac
Joline Barrett <jolinebarrett@Jolines-MacBook-Pro.local>
2015-04-08 00:59:20 -0400

    "complete" recursive version in fortran

---
+++ Recu_Fortran.f95
@@ -0,0 +1,22 @@
+!Program to perform the Ackermann Function 
+!
+program ackMain
+	integer:: m, n, endResult
+	do m = 0, 3
+		do n = 0, 6
+			endResult = ackermann(m,n)
+			write(*,*) m, n, ': ', endResult
+		end do	
+	end do	
+end program ackMain
+recursive Function ackermann(m,n) result(result)
+	integer, intent(in) :: m, n, 
+	integer ::result
+	if(m == 0) then
+		result = n + 1
+	else if (n == 0) then
+		result = ackermann(m-1, 1);
+	else
+		result = ackermann(m-1, ackermann(m, n -1))
+	end if
+end function ackermann
