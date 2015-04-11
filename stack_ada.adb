08207c9db5f062a3c7018246d5fd85ed78154787
Joline Barrett <jolinebarrett@Jolines-MacBook-Pro.local>
2015-04-11 03:00:57 -0400

    in "Complete"
    needs timer and testing

--- stack_ada.adb
+++ stack_ada.adb
@@ -1,5 +1,12 @@
 --Iterative implementation of the Ackermann Function
 --uses stack.adb and stack.ads
+--Contents of stack.ads/adb:
+--    procedure push(x: in integer);
+--    procedure pop(x: out integer);
+--    function stack_is_empty return Boolean;
+--    function stack_top return integer;
+--    procedure reset_stack;
+--    function getStackItem(x: in integer) return integer;
 
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
@@ -7,9 +14,21 @@ with stack; use stack;
 
 procedure stack_ada is
 --
-procedure ackermann (m: in integer, n: in integer, r: out integer) is
+procedure ackermann (m: in integer, n: in out integer) is
 push(m);
-
+while not stack_is_empty() loop
+    pop(m);
+    if m = 0 then
+        n := n + 1;
+    elsif n = 0 then
+        n := 1;
+        push (m-1);
+    else
+        n := n - 1;
+        push(m-1);
+        push(m);
+    end if;
+end loop;
 end ackermann;
 --
 begin
@@ -19,7 +38,7 @@ begin
     put_line("Enter m and n");
     get(m);
     get(n);
-    r = ackermann(m,n,r);
+    r = ackermann(m,n);
     put_line("Result = ");
     put(r);
     new_line;
